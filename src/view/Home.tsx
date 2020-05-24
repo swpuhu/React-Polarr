cd9c748647a2fd42883e284392a14f7357e5cf03
huyunhe <swpujita@126.com>
2020-05-24 14:24:36 +0800

    before eject

---
+++ src/view/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from "styled-components";
+const Wrapper = styled.div`
+    
+`
+
+const Home: React.FC = (props) => {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
