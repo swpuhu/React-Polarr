42c17501e8989240431f9431bf0bc87c3491bc83
swpuhu <swpujita@126.com>
2020-05-28 15:13:56 +0800

    allow build

---
+++ build/precache-manifest.df8b8b4cba06508256ba918e4ea835e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e9e66886551808243e2cc0edf9a7a5",
+    "url": "/8b5bcab159e1c917e266.worker.js"
+  },
+  {
+    "revision": "ee3ea0c39513379c0f0a1044861b1d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6be2b09597d2ce4cba57",
+    "url": "/static/css/main.747de6b2.chunk.css"
+  },
+  {
+    "revision": "11afac84befc78c8cb5a",
+    "url": "/static/js/2.038b8169.chunk.js"
+  },
+  {
+    "revision": "c69016636d013282475d5f577e1d8920",
+    "url": "/static/js/2.038b8169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6be2b09597d2ce4cba57",
+    "url": "/static/js/main.0c6b7312.chunk.js"
+  },
+  {
+    "revision": "3ec3a402d311cfeb71ae",
+    "url": "/static/js/runtime-main.b70c2a92.js"
+  }
+]);
